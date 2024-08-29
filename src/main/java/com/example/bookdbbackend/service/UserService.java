24229a2c270cc3d35b91de82bcb63dbc0a2db581
onboni <onni.luova@gmail.com>
2024-08-29 11:48:31 +0300

    UserService luotu

---
+++ src/main/java/com/example/bookdbbackend/service/UserService.java
@@ -0,0 +1,14 @@
+package com.example.bookdbbackend.service;
+
+import com.example.bookdbbackend.model.User;
+import com.example.bookdbbackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+}
