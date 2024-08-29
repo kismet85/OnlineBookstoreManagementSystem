85a01f309bdb7589062af42f3566ef3c49a0172d
kismet <kismet1985@hotmail.fi>
2024-08-29 11:25:38 +0300

    update structure

--- src/main/java/com/example/bookdbbackend/model/User.java
+++ src/main/java/com/example/bookdbbackend/model/User.java
@@ -1,14 +1,11 @@
 package com.example.bookdbbackend.model;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.NaturalId;
 
 @Entity
 @Getter
@@ -22,7 +19,11 @@ public class User {
         private String lastName;
         private String email;
         private String country;
-
-
+        private int streetNumber;
+        private String streetName;
+        private int phoneNumber;
+        private int postalCode;
+        private String provice;
+        private String role;
 
 }
