ad0d8972f9a213d7234fd422fcb92375d6896650
kismet <kismet1985@hotmail.fi>
2024-08-29 11:43:25 +0300

    service luokat

---
+++ src/main/java/com/example/bookdbbackend/service/UserService.java
@@ -0,0 +1,34 @@
+package com.example.bookdbbackend.service;
+
+import com.example.bookdbbackend.model.User;
+
+import java.util.List;
+
+public class UserService implements IUserService{
+
+
+    @Override
+    public User addUser(User user) {
+        return null;
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return null;
+    }
+
+    @Override
+    public User updateUser(User user, Long id) {
+        return null;
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+
+    }
+}
