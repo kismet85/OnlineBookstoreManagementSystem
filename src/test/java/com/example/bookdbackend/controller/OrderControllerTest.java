7d4b42b856f338bbf27e64804cbf8166a988d905
Jannassl <Jan.nassling@metropolia.fi>
2024-11-06 10:45:00 +0200

    Ordercontrollertest aloitettu

--- src/test/java/com/example/bookdbackend/controller/OrderControllerTest.java
+++ src/test/java/com/example/bookdbackend/controller/OrderControllerTest.java
@@ -1,6 +1,6 @@
-package com.example.bookdbbackend.controller;
+package com.example.bookdbackend.controller;
 
-import com.example.bookdbbackend.model.Order;
+import com.example.bookdbbackend.controller.OrderController;
 import com.example.bookdbbackend.service.IOrderService;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -8,14 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-
-import java.util.Arrays;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
 class OrderControllerTest {
 
