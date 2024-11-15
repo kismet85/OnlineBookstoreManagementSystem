b29d14c07fcea5acf662c035c418bb24cd7197ea
onboni <onni.luova@gmail.com>
2024-11-15 11:12:00 +0200

    SecurityConfigurationTest

---
+++ src/test/java/com/example/configs/SecurityConfigurationTest.java
@@ -0,0 +1,32 @@
+package com.example.configs;
+
+import com.example.bookdbbackend.BookdbbackendApplication;
+import com.example.bookdbbackend.dtos.LoginUserDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(classes = BookdbbackendApplication.class)
+@AutoConfigureMockMvc
+class SecurityConfigurationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void testAdminEndpointsWithoutAuth() throws Exception {
+        mockMvc.perform(get("/admin")).andExpect(status().isForbidden());
+    }
+}
