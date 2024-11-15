5786f2c7539ad3571edad0f0a1e61efbc552e057
onboni <onni.luova@gmail.com>
2024-11-15 10:41:20 +0200

    JwtAuthenticationFilterTest

--- src/main/java/com/example/bookdbbackend/configs/JwtAuthenticationFilter.java
+++ src/main/java/com/example/bookdbbackend/configs/JwtAuthenticationFilter.java
@@ -31,7 +31,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     }
 
     @Override
-    protected void doFilterInternal(
+    public void doFilterInternal(
             @NonNull HttpServletRequest request,
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain
